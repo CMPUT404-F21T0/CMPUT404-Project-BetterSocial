b5535f6b65c869e4b663a5195bd906003758e537
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-10-26 13:54:11 -0600

    chore: add more tests

--- socialdistribution/bettersocial/tests.py
+++ socialdistribution/bettersocial/tests.py
@@ -9,7 +9,7 @@ class AuthorModelTests(TestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        self.testUser: User = User.objects.create(
+        self.user: User = User.objects.create(
             username = 'testuser',
             password = 'testuserpw',
             first_name = 'testuserfn',
@@ -17,9 +17,28 @@ class AuthorModelTests(TestCase):
             email = 'testuser@email.example.com',
         )
 
-        self.testAuthor: Author = self.testUser.author
+        self.author: Author = Author.objects.get(user_id = self.user.id)
 
     def test_display_name(self):
         """Tests that the display name returned by the dynamic property is correct"""
 
-        self.assertEqual(self.testAuthor.display_name, f'{self.testUser.first_name} {self.testUser.last_name}')
+        self.assertEqual(self.author.display_name, f'{self.user.first_name} {self.user.last_name}')
+
+    def test_delete_cascade(self):
+        """Tests that the author object is deleted when the associated user is deleted"""
+
+        # Save user_id to local var because it will be inaccessible after the delete
+        user_id = self.user.id
+
+        # Make sure it exists before delete
+        self.assertTrue(Author.objects.filter(user_id = user_id).exists(), 'Author object associated with the user does not exist!')
+
+        self.user.delete()
+
+        # Make sure it DNE after delete
+        self.assertFalse(Author.objects.filter(user_id = user_id).exists(), 'Author object was not deleted upon user deletion!')
+
+    def test_parent_relation(self):
+        """Tests that the relationship accessor refers to the same user that we set up"""
+
+        self.assertEquals(self.author.user, self.user)
