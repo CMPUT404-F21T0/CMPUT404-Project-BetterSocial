b38e8e40b022dfda49cf412e0e6cf2e521893df3
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-10-26 01:12:04 -0600

    test: add initial test to verify tests work

--- socialdistribution/bettersocial/tests.py
+++ socialdistribution/bettersocial/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from bettersocial.models import Author
+
+
+class AuthorModelTests(TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.testUser: User = User.objects.create(
+            username = 'testuser',
+            password = 'testuserpw',
+            first_name = 'testuserfn',
+            last_name = 'testuserln',
+            email = 'testuser@email.example.com',
+        )
+
+        self.testAuthor: Author = self.testUser.author
+
+    def test_display_name(self):
+        """Tests that the display name returned by the dynamic property is correct"""
+
+        self.assertEqual(self.testAuthor.display_name, f'{self.testUser.first_name} {self.testUser.last_name}')
