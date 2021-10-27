94f58a4983c77b4c467d51550ef1cbae5f6edebb
deasisrj1 <rdeasis@ualberta.ca>
2021-10-26 22:14:12 -0600

    Fix: clean up code, prettier

--- socialdistribution/bettersocial/views.py
+++ socialdistribution/bettersocial/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
-
+from django.utils.decorators import method_decorator
 from django.views import generic
-
 
 class IndexView(generic.base.TemplateView):
     template_name = 'bettersocial/index.html'
@@ -12,8 +10,6 @@ class IndexView(generic.base.TemplateView):
     # TODO: Not sure if this is the proper way to query all users / users post in db
     def users(self):
         return User.objects.all()
-
-
 
 @method_decorator(login_required, name = 'dispatch')
 class ProfileView(generic.base.TemplateView):
