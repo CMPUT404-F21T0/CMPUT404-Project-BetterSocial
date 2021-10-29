5a63b3005ad686866cde29717a7f34e26356af65
shadowciaw <sqli@ualberta.ca>
2021-10-28 23:39:35 -0600

    Added unfriend/unfollow/follow button functionality

--- socialdistribution/bettersocial/urls.py
+++ socialdistribution/bettersocial/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('profile/<uuid:uuid>', views.ProfileView.as_view(), name = 'profile'),
     path('inbox/', views.InboxView.as_view(), name = 'inbox'),
     path('stream/', views.StreamView.as_view(), name = 'stream'),
-   
+
+    path('profile/<uuid:uuid>/<str:action>', views.ProfileActionView.as_view(), name = 'profile_action')
 ]
--- socialdistribution/bettersocial/views.py
+++ socialdistribution/bettersocial/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType as DjangoContentType
 from django.contrib.auth.models import User
+from django.http.response import HttpResponseRedirect
 from django.utils.decorators import method_decorator
+from django.urls import reverse
 from django.views import generic
 from django.db.models import Q
 
@@ -34,8 +36,8 @@ class ProfileView(generic.base.TemplateView):
         if author_uuid == user_uuid:
             context['posts'] = author.post_set.all()
         else:
-            context['author_following_user'] = Following.objects.filter(author=author_uuid, following_uuid=user_uuid)
-            context['user_following_author']= Following.objects.filter(author=user_uuid, following_uuid=author_uuid)
+            context['author_following_user'] = bool(Following.objects.filter(author=author_uuid, following_uuid=user_uuid))
+            context['user_following_author']= bool(Following.objects.filter(author=user_uuid, following_uuid=author_uuid))
 
             # TODO: Might only need to have Public posts to be queried or publick and friends posts?
             context['posts'] = Post.objects.filter(
@@ -44,6 +46,17 @@ class ProfileView(generic.base.TemplateView):
                 (Q(visibility = Post.Visibility.PRIVATE) & Q(recipient_uuid = user_uuid))).order_by('-published')
         
         return context
+
+# CODE REFERENCED: https://stackoverflow.com/questions/54187625/django-on-button-click-call-function-view
+@method_decorator(login_required, name = 'dispatch')
+class ProfileActionView(generic.View):
+    def post(self, request, uuid, action, *args, **kwargs):
+        author = Author.objects.filter(uuid = request.user.author.uuid).get()
+        if action == 'follow':
+            Following.objects.create(following_uuid=uuid, author=author)
+        elif action == 'unfollow':
+            Following.objects.filter(following_uuid=uuid, author=author).delete()
+        return HttpResponseRedirect(reverse('bettersocial:profile', args=(uuid,)))
 
 @method_decorator(login_required, name = 'dispatch')
 class InboxView(generic.ListView):
