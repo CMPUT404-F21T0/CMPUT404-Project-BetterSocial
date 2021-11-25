a5e194fed869b55f12974b933462e717b39c25bf
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-11-25 00:46:34 -0700

    feat: add Team4 adapter

--- socialdistribution/api/adapters.py
+++ socialdistribution/api/adapters.py
@@ -1,17 +1,26 @@
 from typing import Dict
 
+from rest_framework.request import Request
+
 
 class BaseAdapter:
     """Default handlers for a given node connection. Methods should be overridden for each team, as needed."""
-    pass
+
+    def postInboxItem(self, request: Request, *args, **kwargs) -> Request:
+        return request
 
 
 class Team1Adapter(BaseAdapter):
     pass
 
 
+class Team4Adapter(BaseAdapter):
+    pass
+
+
 # A global list of adapters that are tied to nodes via the database.
 registered_adapters: Dict[str, BaseAdapter] = {
     'default': BaseAdapter(),
-    'team_1': Team1Adapter()
+    'team_1': Team1Adapter(),
+    'team_4': Team1Adapter(),
 }
