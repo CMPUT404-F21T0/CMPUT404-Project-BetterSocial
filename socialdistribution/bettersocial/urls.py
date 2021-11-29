41a32cc1a0baab2b687e1018c39dd0096ecf6518
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-11-28 18:25:41 -0700

    feat: add custom uuid converter

--- socialdistribution/bettersocial/urls.py
+++ socialdistribution/bettersocial/urls.py
@@ -1,27 +1,29 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, register_converter
 
-from . import views
+from . import views, converters
+
+register_converter(converters.BetterUUIDConverter, 'buuid')
 
 app_name = 'bettersocial'
 
 urlpatterns = [
     path('', views.StreamView.as_view(), name = 'index'),
     path('post/', views.AddPostView.as_view(), name = 'post'),
-    path('article/<uuid:pk>/', views.ArticleDetailView.as_view(), name = 'article_details'),
-    path('article/<uuid:pk>/comment/', views.AddCommentView.as_view(), name = 'add_comment'),
-    path('article/<uuid:pk>/likes/', views.PostLikesView.as_view(), name = 'post_likes_list'),
-    path('article/<uuid:pk>/remove/', views.DeletePostView.as_view(), name = 'delete_post'),
-    path('article/edit/<uuid:pk>/', views.UpdatePostView.as_view(), name = 'edit_post'),
-    path('profile/<uuid:uuid>', views.ProfileView.as_view(), name = 'profile'),
-    path('profile/<uuid:uuid>/<str:action>', views.ProfileActionView.as_view(), name = 'profile_action'),
+    path('article/<buuid:pk>/', views.ArticleDetailView.as_view(), name = 'article_details'),
+    path('article/<buuid:pk>/comment/', views.AddCommentView.as_view(), name = 'add_comment'),
+    path('article/<buuid:pk>/likes/', views.PostLikesView.as_view(), name = 'post_likes_list'),
+    path('article/<buuid:pk>/remove/', views.DeletePostView.as_view(), name = 'delete_post'),
+    path('article/edit/<buuid:pk>/', views.UpdatePostView.as_view(), name = 'edit_post'),
+    path('profile/<buuid:uuid>', views.ProfileView.as_view(), name = 'profile'),
+    path('profile/<buuid:uuid>/<str:action>', views.ProfileActionView.as_view(), name = 'profile_action'),
     path('inbox/', views.InboxView.as_view(), name = 'inbox'),
     path('friends/', views.FollowersView.as_view(), name = 'friends'),
     path('delete-following/', views.DeleteFollowingView.as_view(), name = 'delete_following'),
     path('add-following/', views.CreateFollowingView.as_view(), name = 'add_following'),
-    path('article/<uuid:pk>/share/', views.SharePostView.as_view(), name= 'share_post'),
-    path('article/<uuid:uuid>/share/<str:action>', views.SharePostActionView.as_view(), name= 'share_post_action'),
+    path('article/<buuid:pk>/share/', views.SharePostView.as_view(), name = 'share_post'),
+    path('article/<buuid:uuid>/share/<str:action>', views.SharePostActionView.as_view(), name = 'share_post_action'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
