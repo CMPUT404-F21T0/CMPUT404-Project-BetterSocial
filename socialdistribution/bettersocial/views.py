7424a7d298165f6968ac6bcf8d7cdd94420780a4
shadowciaw <sqli@ualberta.ca>
2021-12-01 23:02:54 -0700

    WIP Address comments

--- socialdistribution/bettersocial/views.py
+++ socialdistribution/bettersocial/views.py
@@ -152,6 +152,12 @@ class ProfileView(generic.base.TemplateView):
 
         if author_uuid == user_uuid:
             context['posts'] = author.post_set.all()
+
+            # TODO: Might only need to have Public posts to be queried or publick and friends posts?
+            context['posts'] = Post.objects.filter(
+                (Q(visibility = Post.Visibility.PUBLIC) & Q(author__uuid = author_uuid)) |
+                (Q(visibility = Post.Visibility.FRIENDS) & Q(author__follower__follower_uuid = user_uuid) & Q(author__following__following_uuid = user_uuid)) |
+                (Q(visibility = Post.Visibility.PRIVATE) & Q(recipient_uuid = user_uuid))).distinct().order_by('-published')
         else:
             context['author_following_user'] = bool(Following.objects.filter(author = author_uuid, following_uuid = user_uuid))
             if Author.objects.filter(uuid = user_uuid).exists():
@@ -159,7 +165,7 @@ class ProfileView(generic.base.TemplateView):
             else:
                 # TODO: make sure this works
                 node = Node.objects.filter(host__contains = self.request.GET['host']).get()
-                url = yarl.URL(node.host + '/author/' + author_uuid + '/followers/' + user_uuid).human_repr()
+                url = (yarl.URL(node.host) / 'author' / author_uuid / 'followers' / user_uuid / '').human_repr()
                 response = requests.get(
                     url,
                     headers = { 'Accept': 'application/json' },
@@ -171,12 +177,9 @@ class ProfileView(generic.base.TemplateView):
                     context['user_following_author'] = bool(Following.objects.filter(author = user_uuid, following_uuid = author_uuid))
                 else:
                     context['user_following_author'] = False
-
-            # TODO: Might only need to have Public posts to be queried or publick and friends posts?
-            context['posts'] = Post.objects.filter(
-                (Q(visibility = Post.Visibility.PUBLIC) & Q(author__uuid = author_uuid)) |
-                (Q(visibility = Post.Visibility.FRIENDS) & Q(author__follower__follower_uuid = user_uuid) & Q(author__following__following_uuid = user_uuid)) |
-                (Q(visibility = Post.Visibility.PRIVATE) & Q(recipient_uuid = user_uuid))).distinct().order_by('-published')
+            
+            # TODO: Get remote author posts
+            # context['posts'] = None
 
         return context
 
@@ -199,17 +202,25 @@ class ProfileActionView(generic.View):
 
             author_serialized = AuthorSerializer(author, context = { 'request': self.request }).data
             follower_json = {
-                'type': 'author',
-                'id': author_serialized['id'],
-                'url': author_serialized['url'],
-                'host': author_serialized['host'],
-                'displayName': author.display_name,
-                'github': author.github_url,
-                'profileImage': author_serialized['profileImage'] or ''
+                'type': 'Follow',
+                'summary': f'{author_serialized.displayName} wants to follow user at {uuid}',
+                'actor': {
+                    'type': 'author',
+                    'id': author_serialized.id,
+                    'url': author_serialized.url,
+                    'host': author_serialized.host,
+                    'displayName': author_serialized.displayName,
+                    'github': '',
+                    'profileImage': ''
+                },
+                'object': {
+                    # their user
+                    'type': 'author',
+                }
             }
 
             # TODO: need foreign author uuid with remote host url
-            url = (yarl.URL(node.host) / node.prefix / 'author' / author.uuid / 'followers' / uuid).human_repr()
+            url = (yarl.URL(node.host) / node.prefix / 'author' / author.uuid / 'inbox' / '').human_repr()
             response = requests.post(
                 url,
                 headers = { 'Accept': 'application/json' },
