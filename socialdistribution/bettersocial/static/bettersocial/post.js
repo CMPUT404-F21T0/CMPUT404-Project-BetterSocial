c2e49bae1847333fb17e17043f791dd47fc3b50d
nmrchvz <npchavez@ualberta.ca>
2021-12-02 17:13:01 -0700

    fixed tag input when editing post of existing tags

--- socialdistribution/bettersocial/static/bettersocial/post.js
+++ socialdistribution/bettersocial/static/bettersocial/post.js
@@ -1,21 +1,21 @@
 var catagory_list = new Array()
-
 function enterTagFunction() {
-    var new_catagory_list = catagory_list.concat(JSON.parse(document.getElementById("initial-id_categories").value))
-    var tag = prompt("Enter a catagory tag", "blog");
-    
-    if (tag != null && !new_catagory_list.includes(tag) ) {
-        new_catagory_list.push(tag)
+    var person = prompt("Enter a catagory tag", "blog");
+    var current_list =  JSON.parse("[" + document.getElementById("initial-id_categories").value + "]"); 
+    current_list = current_list.toString().split(",")
+    if (person != null && !current_list.includes(person) ) {
+        current_list.push(person)
         var string_catagory = ''
-        for (var i = 0; i < new_catagory_list.length; i++){
-            string_catagory += '"'+new_catagory_list[i] + '"'
-            if (i != new_catagory_list.length - 1){
+        for (var i = 0; i < current_list.length; i++){
+            string_catagory += '"'+current_list[i] + '"'
+            if (i != current_list.length - 1){
                 string_catagory += ","
             }
         }
         document.getElementById("id_categories").innerHTML = '[' + string_catagory +']';
     }
-    else if (new_catagory_list.includes(tag)) {
+    else
+    {
         alert("That category tag already exists!");
     }
 }
